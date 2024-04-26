d9d81f1f8ce6a7cbd0170586e95be196098cd44c
Nguyen Kim Dien <trietnk01@gmail.com>
2024-04-26 14:07:43 +0700

    Update code layout form

---
+++ stack-fe/src/app/(pages)/admin/news/form/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewsFormlayout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  return <React.Fragment>{children}</React.Fragment>;
+};
+
+export default NewsFormlayout;
