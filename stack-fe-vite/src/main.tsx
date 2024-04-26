bb144e65ee9d593d6bd067b4f941e75a4d37bd90
Nguyen Kim Dien <trietnk01@gmail.com>
2024-04-26 21:51:23 +0700

    Update typescript json autoimport allowSyntheticDefaultImports

--- stack-fe-vite/src/main.tsx
+++ stack-fe-vite/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/index.ts";
+import { store } from "./store";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
