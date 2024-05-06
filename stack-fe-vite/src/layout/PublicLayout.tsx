018d07dbdc98a39db5d514bf54f6d3980cf2a951
Nguyen Kim Dien <trietnk01@gmail.com>
2024-05-06 18:59:06 +0700

    Update code public homepage

--- stack-fe-vite/src/layout/PublicLayout.tsx
+++ stack-fe-vite/src/layout/PublicLayout.tsx
@@ -1,12 +1,34 @@
+import { SearchOutlined } from "@ant-design/icons";
+import { Select } from "antd";
 import { Outlet } from "react-router-dom";
-
-import React from "react";
-
+import styles from "@/assets/scss/public-layout.module.scss";
 const PublicLayout = () => {
+  const handleCategoryNewsChange = () => {};
   return (
-    <React.Fragment>
-      <Outlet />
-    </React.Fragment>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Papaya News</h1>
+        <div className={styles.searchArea}>
+          <div className={styles.searchBox}>
+            <SearchOutlined className={styles.seachIcon} />
+            <input
+              type="text"
+              name="keyword"
+              placeholder="Search..."
+              className={styles.searchInput}
+            />
+          </div>
+          <Select
+            size="large"
+            defaultValue=""
+            className={styles.categoryNewsId}
+            options={[]}
+            onChange={handleCategoryNewsChange}
+          />
+        </div>
+        <Outlet />
+      </div>
+    </div>
   );
 };
 
--- stack-fe-vite/src/pages/public/HomePage.tsx
+++ stack-fe-vite/src/pages/public/HomePage.tsx
@@ -1,7 +1,69 @@
 import React from "react";
-
+import styles from "@/assets/scss/public-layout.module.scss";
 const HomePage = () => {
-  return <React.Fragment>HomePage</React.Fragment>;
+  return (
+    <React.Fragment>
+      <div className={styles.newsBox}>
+        <div className={styles.itemNews}>
+          <a href="javascript:void(0);">
+            <img src="/kinh-te-1.jpg" className={styles.itemImg} width={350} />
+          </a>
+          <div className={styles.itemInfo}>
+            <h3 className={styles.titleCategory}>Pháp luật</h3>
+            <h2 className={styles.titleNews}>
+              <a href="javascript:void(0);">
+                Tài xế xe đầu kéo vi phạm nồng độ cồn, đâm vào nhà dân
+              </a>
+            </h2>
+            <div className={styles.introNews}>
+              Lái xe đầu kéo có nồng độ cồn 0,5 mg/lít khí thở, đâm vào nhà dân bên đường khiến một
+              người chết, một người nguy kịch, 5 người bị thương.
+            </div>
+          </div>
+        </div>
+        <div className={styles.itemNews}>
+          <a href="javascript:void(0);">
+            <img src="/kinh-te-2.jpg" className={styles.itemImg} width={350} />
+          </a>
+          <div className={styles.itemInfo}>
+            <h3 className={styles.titleCategory}>Pháp luật</h3>
+            <h2 className={styles.titleNews}>
+              <a href="javascript:void(0);">
+                Tài xế xe đầu kéo vi phạm nồng độ cồn, đâm vào nhà dân
+              </a>
+            </h2>
+            <div className={styles.introNews}>
+              Lái xe đầu kéo có nồng độ cồn 0,5 mg/lít khí thở, đâm vào nhà dân bên đường khiến một
+              người chết, một người nguy kịch, 5 người bị thương.
+            </div>
+          </div>
+        </div>
+        <div className={styles.itemNews}>
+          <a href="javascript:void(0);">
+            <img src="/kinh-te-3.jpg" className={styles.itemImg} width={350} />
+          </a>
+          <div className={styles.itemInfo}>
+            <h3 className={styles.titleCategory}>Pháp luật</h3>
+            <h2 className={styles.titleNews}>
+              <a href="javascript:void(0);">
+                Tài xế xe đầu kéo vi phạm nồng độ cồn, đâm vào nhà dân
+              </a>
+            </h2>
+            <div className={styles.introNews}>
+              Lái xe đầu kéo có nồng độ cồn 0,5 mg/lít khí thở, đâm vào nhà dân bên đường khiến một
+              người chết, một người nguy kịch, 5 người bị thương.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.pagination}>
+        <button className={styles.page}>1</button>
+        <button className={styles.page}>2</button>
+        <button className={styles.page}>3</button>
+        <button className={styles.page}>4</button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default HomePage;
