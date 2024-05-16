3c813d31760384e286b21874619fa9750a3eae1d
Nguyen Kim Dien <trietnk01@gmail.com>
2024-05-16 12:16:51 +0700

    Update 88423

--- stack-be/src/main.ts
+++ stack-be/src/main.ts
@@ -8,11 +8,7 @@ import cookieParser from "cookie-parser";
 import { graphqlUploadExpress } from "graphql-upload-ts";
 import { AppModule } from "@/app.module";
 async function bootstrap() {
-  const httpsOptions = {
-    cert: fs.readFileSync(join(process.cwd() + "/src/secrets/certificate.pem")),
-    key: fs.readFileSync(join(process.cwd() + "/src/secrets/private.pem"))
-  };
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   const confService = app.get(ConfigService);
   app.useStaticAssets(join(__dirname, "..", "public"));
